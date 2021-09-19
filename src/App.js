04c7e88bfa3c76befae0cd4ddfa6b7d3610fb02b
sharif764 <ss1884088@gmail.com>
2021-09-19 22:58:59 +0600

    JJ

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
-import { useEffect, useState } from 'react';
+/* import { useEffect, useState } from 'react'; */
 
 function App() {
   return (
     <div className="App">
-      <Countries></Countries>
+      {/* <Countries></Countries> */}
     </div>
   );
 }
-function Countries() {
+
+
+
+
+/* function Countries() {
   const [countries, setCountries] = useState([]);
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
@@ -20,7 +24,16 @@ function Countries() {
     <div>
       <h1> Traveling Around The world</h1>
       <h4>Countries Available {countries.length}</h4>
+      {countries.map(country => <Country name={country.name} capital={country.capital}></Country>)}
     </div>
   )
 }
+function Country(props) {
+  return (
+    <div>
+      <h2>Name : {props.name}</h2>
+      <h2>Capital :{props.capital}</h2>
+    </div>
+  )
+} */
 export default App;
